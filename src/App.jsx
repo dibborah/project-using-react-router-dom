86f387fa64c94c95cf3e645db3c2fb5e987b404b
dibya.borah <dibyajyotiborah469@gmail.com>
2024-04-22 22:22:28 +0530

    .env files is set . contants.js files is set . And router setup is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { apiKey } from "./contants";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
 const App = () => {
-  return <div>App</div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<h1>Parent Element</h1>}>
+        <Route path={"/"} element={<h1>Home Page</h1>} />
+        <Route path={"/about"} element={<h1>About Page</h1>} />
+        <Route path={"/contact"} element={<h1>Contact Page</h1>} />
+      </Route>
+    )
+  );
+  console.log(apiKey);
+  return (
+    <RouterProvider router={router}>
+      <h1>App</h1>
+    </RouterProvider>
+  );
 };
 
 export default App;
